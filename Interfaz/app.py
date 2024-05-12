eaa4cf4efe0e83760675bfa5fa154a893e888db7
oscar <oscar.martz511@gmail.com>
2024-05-11 23:48:20 -0600

    Creacion de ApartaGuest

---
+++ Interfaz/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from pymysql import connect
+
+app = Flask(__name__)
+
+# Configurar la base de datos
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Frutillita12@localhost:3306/apartaguest'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Modelado de las tablas
+class apartamento(db.Model):
+    id_inquilino = db.Column(db.Integer, primary_key=True)
+    nombre_edificio = db.Column(db.String(255), nullable=False)
+    numero_apartamento = db.Column(db.Integer, unique=True, nullable=False)
+    piso = db.Column(db.Integer, nullable=False)
+    tamaño = db.Column(db.Integer, nullable=False)
+    precio_alquiler = db.Column(db.Double(10,2), nullable=False)
+    disponibilidad = db.Column(db.)
